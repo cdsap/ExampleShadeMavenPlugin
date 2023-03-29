9a1efb0e9f37f86ba921a835326facec70425596
Inaki Villar <inaki.seri@gmail.com>
2023-03-29 16:06:40 -0700

    initial import

---
+++ src/main/java/io/github/cdsap/App.java
@@ -0,0 +1,13 @@
+/* (C)2015 */
+package io.github.cdsap;
+
+/** Messy code makes programmers life harder. */
+public class App {
+
+  public static final String myConst = "a";
+
+  public static void main(String[] args) {
+    System.out.println(myConst);
+    System.out.println("Please " + "" + "fix" + " me :)");
+  }
+}
