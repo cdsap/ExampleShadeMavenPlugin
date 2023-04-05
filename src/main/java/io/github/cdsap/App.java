b0d289d8f99bd4ea9cd6606da7a7816a2aebbf65
Inaki Villar <inaki.seri@gmail.com>
2023-04-04 17:15:49 -0700

    forcing the input

--- src/main/java/io/github/cdsap/App.java
+++ src/main/java/io/github/cdsap/App.java
@@ -8,6 +8,6 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println(myConst);
-    System.out.println("Please " + "" + "fiwx" + " me :)");
+    System.out.println("Please " + "" + "fix" + " me :)");
   }
 }
